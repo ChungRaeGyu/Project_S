9ac5a67b865d3d05a67d4fa14d47cc49797c3292
Chung Rae Gyu <wogns9769@gmail.com>
2026-06-24 11:43:52 +0900

    StartScene
    끝

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,36 @@
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    public static NetworkManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void ConnectButton()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Master Server");
+        PhotonNetwork.JoinLobby();
+    }
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+        PhotonNetwork.LoadLevel("LobbyScene");
+    }
+}
