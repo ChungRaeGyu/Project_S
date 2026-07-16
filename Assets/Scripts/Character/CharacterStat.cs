704a49ee558ac12fdb9e11374cdd50e865e0b9a8
Chung Rae Gyu <wogns9769@gmail.com>
2026-07-16 22:32:10 +0900

    Update

--- Assets/Scripts/Character/CharacterStat.cs
+++ Assets/Scripts/Character/CharacterStat.cs
@@ -1,12 +1,17 @@
 using UnityEngine;
 
-public class CharacterStat : MonoBehaviour
+public class CharacterStat : MonoBehaviour, IDamageable
 {
     //여기서 이제 어떤 장비를 가지고 있는 지 확인하고 
     public GameObject[] equips = new GameObject[2]; //장비
     IInteractable currentInteractable;
     GameObject currentItem; //보고 있는 장비
 
+    public bool IsDead => throw new System.NotImplementedException();
+    public void TakeDamage(int amount, GameObject source)
+    {
+        throw new System.NotImplementedException();
+    }
     public void ChangeItemInteract(GameObject obj)
     {
         currentItem = obj;
