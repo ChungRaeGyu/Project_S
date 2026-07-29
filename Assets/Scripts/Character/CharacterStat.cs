5ce1a32e3a427ef0a8b44730bb393e734c94c381
Chung Rae Gyu <wogns9769@gmail.com>
2026-07-29 11:18:17 +0900

    일단 임시 됌

--- Assets/Scripts/Character/CharacterStat.cs
+++ Assets/Scripts/Character/CharacterStat.cs
@@ -14,13 +14,67 @@ public class CharacterStat : MonoBehaviour, IDamageable
     [SerializeField] float fearturm = 1f;
     [SerializeField] int fearmount = 1;
     public event Action Onfear;
+
+    [SerializeField] private int deadCount = 0;
     
     public float speed = 10f;
 
     public bool IsDead => throw new System.NotImplementedException();
+
+    private void Update()
+    {
+        if (hp <= 0)
+        {
+            Dead();
+            hp = 100;
+        }
+    }
     public void TakeDamage(int amount, GameObject source)
     {
-        throw new System.NotImplementedException();
+        //뭐 어쩌다가 
+        if (hp <= 0)
+        {
+            Dead();
+        }
+    }
+    public void Dead()
+    {
+        StartCoroutine(CDead());//FindObjects를 안쓰는 방법이 있긴 한데 귀찮..
+        //안쓸 수 있는 방법 각 EchoObject에서 생성됐을때 각자 어딘가에 보관을 한다. 그리고 죽었을때 invoke로 호출해버리면 되긴함
+    }
+
+    IEnumerator CDead()
+    {
+        int batchSize = 20;
+        if (deadCount == 0)
+        {
+            //여기선 눈이 안보이도록 해야하는데 음;; 근데 음..
+            //건물과 모든 플레이어를 안보이도록 해야한단 말이지
+            //제일 간단한법
+            EchoObject[] objects = FindObjectsByType<EchoObject>();
+            for (int i = 0; i < objects.Length; i++)
+            {
+                objects[i].SetReveal(0);
+                if (i % batchSize == 0)
+                {
+                    yield return null;
+                }
+            }
+            hp = 100;
+        }
+        else
+        {
+            EchoObject[] objects = FindObjectsByType<EchoObject>();
+            for (int i = 0; i < objects.Length; i++)
+            {
+                objects[i].SetReveal(1);
+                if (i % batchSize == 0)
+                {
+                    yield return null;
+                }
+            }
+        }
+        deadCount++;
     }
     public void ChangeItemInteract(GameObject obj)
     {
